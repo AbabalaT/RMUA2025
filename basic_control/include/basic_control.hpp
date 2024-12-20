8be9409e7b1f6ce37c6116e234b5167a8219f586
TC <Kevin3617@outlook.com>
2024-12-20 20:33:43 +0800

    basic control

---
+++ basic_control/include/basic_control.hpp
@@ -0,0 +1,48 @@
+//
+// Created by tc on 24-12-20.
+//
+#ifndef BASIC_CONTROL_HPP
+#define BASIC_CONTROL_HPP
+
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include "airsim_ros/VelCmd.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "airsim_ros/RotorPWM.h"
+#include <sensor_msgs/Imu.h>
+#include <std_msgs/Float32.h>
+
+#endif //BASIC_CONTROL_HPP
+
+class BasicControl {
+private:
+    airsim_ros::RotorPWM pwm_cmd;
+
+    ros::Subscriber rc_channel1_suber;
+    ros::Subscriber rc_channel2_suber;
+    ros::Subscriber rc_channel3_suber;
+    ros::Subscriber rc_channel4_suber;
+    ros::Subscriber rc_channel5_suber;
+    ros::Subscriber rc_channel6_suber;
+
+    ros::Subscriber imu_suber;
+
+    ros::Publisher pwm_publisher;
+
+    ros::Timer rc_mode_timer;
+
+    void channel1_callback(const std_msgs::Float32::ConstPtr& msg);
+    void channel2_callback(const std_msgs::Float32::ConstPtr& msg);
+    void channel3_callback(const std_msgs::Float32::ConstPtr& msg);
+    void channel4_callback(const std_msgs::Float32::ConstPtr& msg);
+    void channel5_callback(const std_msgs::Float32::ConstPtr& msg);
+    void channel6_callback(const std_msgs::Float32::ConstPtr& msg);
+
+    void rc_mode_check_callback(const ros::TimerEvent& event);
+
+    void imuCallback(const sensor_msgs::Imu::ConstPtr& msg);
+
+public:
+     BasicControl(ros::NodeHandle *nh);
+     ~BasicControl();
+};
---
+++ basic_control/src/basic_control.cpp
@@ -0,0 +1,508 @@
+//
+// Created by tc on 24-12-20.
+//
+#include "basic_control.hpp"
+#include <cmath>
+
+float rc_channel[6] = {0.0, 0.0, -1000.0, 0.0, 0.0, 0.0};
+int ctrl_mode = 0;
+int rc_mode = 0;
+
+typedef struct {
+    float w, x, y, z;
+} Quaternion;
+
+float mat_pid[4][4];	//R-P-Y-throttle
+float angle_pid_mat[3][3];
+
+float throttle_set = 0.0f;
+
+float kalman_q = 5000.0f;
+float kalman_r = 10000000.0f;
+
+float u_real_roll = 0.0f;
+float u_real_pitch = 0.0f;
+float u_real_yaw = 0.0f;
+
+float gyro_data[3], angle_data[3];
+
+float w_yaw_world[3];
+float w_yaw_body[3];
+
+float motor_idle_speed = 0.0;
+float motor_max_speed = 1000.0;
+
+float pid_N = 0.5f;//离散pid微分低通滤波
+
+float kalman_roll(float measure){
+	static float x;
+	static float p;
+	static float k;
+	p = p + kalman_q;
+	k = p / (p + kalman_r);
+	x = x + k * (measure - x);
+	p = (1.0f - k) * p;
+	return x;	
+}
+
+float kalman_pitch(float measure){
+	static float x;
+	static float p;
+	static float k;
+	p = p + kalman_q;
+	k = p / (p + kalman_r);
+	x = x + k * (measure - x);
+	p = (1.0f - k) * p;
+	return x;	
+}
+
+float kalman_yaw(float measure){
+	static float x;
+	static float p;
+	static float k;
+	p = p + kalman_q;
+	k = p / (p + kalman_r);
+	x = x + k * (measure - x);
+	p = (1.0f - k) * p;
+	return x;	
+}
+
+void pid_init(void){
+	mat_pid[0][0] = 0.0;
+	mat_pid[0][1] = 12.5;
+    mat_pid[0][2] = 1.0;
+	mat_pid[0][3] = 10.0;
+	
+	mat_pid[1][0] = 0.0;
+	mat_pid[1][1] = 12.5f;//697.6f;
+	mat_pid[1][2] = 1.0;
+	mat_pid[1][3] = 10.0;
+	
+	mat_pid[2][0] = 0.0;
+	mat_pid[2][1] = 100.0f;//139.53f;
+	mat_pid[2][2] = 12.0f;
+	mat_pid[2][3] = 50.0;
+	
+	angle_pid_mat[0][0] = 2.0;
+	angle_pid_mat[0][1] = 0.0f;//0.00006;//232.55f;
+	angle_pid_mat[0][2] = 0.05f;
+
+	angle_pid_mat[1][0] = 2.0;
+	angle_pid_mat[1][1] = 0.0f;//0.00002f;//697.6f;
+	angle_pid_mat[1][2] = 0.01f;
+	
+	angle_pid_mat[2][0] = 1.8;
+	angle_pid_mat[2][1] = 0.0f;//0.000045f;//139.53f;
+	angle_pid_mat[2][2] = 0.06f;
+}
+
+float pid_roll(float target, float real){
+	static float error;
+	static float sum;
+	static float pre_error;
+	static float result;
+	static float error_rate;
+	static float d_out_1;
+	static float d_out;
+	static float d_error;
+
+	error = target - real;
+	sum = sum + error;
+
+	if(sum > 1000.0f){
+		sum = 1000.0;
+	}
+	if(sum < -1000.0f){
+		sum = -1000.0;
+	}
+	if(error > 3.14f){
+		sum = 0.0f;
+	}
+	if(error < -3.14f){
+		sum = 0.0f;
+	}
+	if(throttle_set < 0.010f){
+		sum = 0.0f;
+	}
+
+	d_error = 0.0f - real;
+	error_rate = d_error - pre_error;
+	pre_error = d_error;
+
+	d_out =  pid_N * error_rate + (1.0f - pid_N) * d_out_1;
+	d_out_1 = d_out;
+//    std::cout<<throttle_set<<std::endl;
+//	std::cout<<target<<" "<<error<<" "<<sum<<" "<<d_out<<std::endl;
+	result = mat_pid[0][0]*target + mat_pid[0][1]*error + mat_pid[0][2]*sum*0.01 + mat_pid[0][3]*d_out*100.0;
+	return result;
+}
+
+float pid_pitch(float target, float real){
+	static float error;
+	static float sum;
+	static float pre_error;
+	static float result;
+	static float error_rate;
+	static float d_out_1;
+	static float d_out;
+	static float d_error;
+
+	error = target - real;
+	sum = sum + error;
+
+	if(sum > 1000.0f){
+		sum = 1000.0;
+	}
+	if(sum < -1000.0f){
+		sum = -1000.0;
+	}
+	if(error > 3.14f){
+		sum = 0.0f;
+	}
+	if(error < -3.14f){
+		sum = 0.0f;
+	}
+	if(throttle_set < 0.010f){
+		sum = 0.0f;
+	}
+
+	d_error = 0.0f - real;
+	error_rate = d_error - pre_error;
+	pre_error = d_error;
+
+	d_out =  pid_N * error_rate + (1.0f - pid_N) * d_out_1;
+	d_out_1 = d_out;
+
+	result = mat_pid[1][0]*target + mat_pid[1][1]*error + mat_pid[1][2]*sum*0.01 + mat_pid[1][3]*d_out*100.0;
+	return result;
+}
+
+float pid_yaw(float target, float real){
+	static float error;
+	static float sum;
+	static float pre_error;
+	static float result;
+	static float error_rate;
+	static float d_out_1;
+	static float d_out;
+	static float d_error;
+
+	error = target - real;
+	sum = sum + error;
+
+	if(sum > 1000.0f){
+		sum = 1000.0;
+	}
+	if(sum < -1000.0f){
+		sum = -1000.0;
+	}
+	if(error > 3.14f){
+		sum = 0.0f;
+	}
+	if(error < -3.14f){
+		sum = 0.0f;
+	}
+	if(throttle_set < 0.010f){
+		sum = 0.0f;
+	}
+
+	d_error = 0.0f - real;
+	error_rate = d_error - pre_error;
+	pre_error = d_error;
+
+	d_out =  pid_N * error_rate + (1.0f - pid_N) * d_out_1;
+	d_out_1 = d_out;
+
+	result = mat_pid[2][0]*target + mat_pid[2][1]*error + mat_pid[2][2]*sum*0.01 + mat_pid[2][3]*d_out*100.0;
+	return result;
+}
+
+float output_roll;
+float output_pitch;
+float output_yaw;
+float imu_roll;
+float imu_pitch;
+float imu_yaw;
+float target_velocity_roll = 0.0f;
+float target_velocity_pitch = 0.0f;
+float target_velocity_yaw = 0.0f;
+
+Quaternion yaw_to_quaternion(double yaw) {
+    Quaternion quaternion;
+    quaternion.w = cos(yaw / 2);
+    quaternion.x = 0;
+    quaternion.y = 0;
+    quaternion.z = sin(yaw / 2);
+    return quaternion;
+}
+
+// Function to convert pitch angle (rotation around Y-axis) to quaternion
+Quaternion pitch_to_quaternion(double pitch) {
+    Quaternion quaternion;
+    quaternion.w = cos(pitch / 2);
+    quaternion.x = sin(pitch / 2);
+    quaternion.y = 0;
+    quaternion.z = 0;
+    return quaternion;
+}
+
+Quaternion roll_to_quaternion(double roll) {
+    Quaternion quaternion;
+    quaternion.w = cos(roll / 2);
+    quaternion.x = 0;
+    quaternion.y = sin(roll / 2);
+    quaternion.z = 0;
+    return quaternion;
+}
+
+Quaternion multiply_quaternion(Quaternion *q1, Quaternion *q2) {
+
+    Quaternion result;
+
+		if(q1->w < 0.0f){
+			q1->w = -1.0f * q1->w;
+			q1->x = -1.0f * q1->x;
+			q1->y = -1.0f * q1->y;
+			q1->z = -1.0f * q1->z;
+		}
+
+		if(q2->w < 0.0f){
+			q2->w = -1.0f * q2->w;
+			q2->x = -1.0f * q2->x;
+			q2->y = -1.0f * q2->y;
+			q2->z = -1.0f * q2->z;
+		}
+
+    result.w = q1->w * q2->w - q1->x * q2->x - q1->y * q2->y - q1->z * q2->z;
+    result.x = q1->w * q2->x + q1->x * q2->w + q1->y * q2->z - q1->z * q2->y;
+    result.y = q1->w * q2->y - q1->x * q2->z + q1->y * q2->w + q1->z * q2->x;
+    result.z = q1->w * q2->z + q1->x * q2->y - q1->y * q2->x + q1->z * q2->w;
+
+		if(result.w < 0.0f){
+			result.w = -result.w;
+			result.x = -result.x;
+			result.y = -result.y;
+			result.z = -result.z;
+		}
+
+    return result;
+}
+
+Quaternion quaternion_conjugate(Quaternion q) {
+    Quaternion result = {q.w, -q.x, -q.y, -q.z};
+    return result;
+}
+
+Quaternion quaternion_diff(Quaternion q1, Quaternion q2) {
+		q1 = quaternion_conjugate(q1);
+    Quaternion result = multiply_quaternion(&q2, &q1);
+		if(result.w < 0.0f){
+			result.w = -result.w;
+			result.x = -result.x;
+			result.y = -result.y;
+			result.z = -result.z;
+		}
+    return result;
+}
+
+void quaternionToAngles(Quaternion q, float *roll, float *pitch, float *yaw) {
+	float we = q.w;
+	if(we > 0.999999f){
+		we = 0.999999f;
+	}
+	if(we < -0.999999f){
+		we = -0.999999f;
+	}
+	float theta = 2.0f * acosf(we);
+	float ne = sqrtf(1.0f - we * we);
+	float nx = q.x / ne;
+	float ny = q.y / ne;
+	float nz = q.z / ne;
+	*pitch = ny * theta;
+	*roll = nx * theta;
+	*yaw = nz * theta;
+}
+
+float euler_angle[3];
+float error_angle[3];
+float error_body[3];
+Quaternion target_quaternion;
+Quaternion measure_quaternion;
+float target_yaw = 0.0f;
+
+void World_to_Body(float *vector_e, float *vector_v,Quaternion Qin)
+{
+	float C11,C12,C13;
+	float C21,C22,C23;
+	float C31,C32,C33;
+
+	float Q[4];
+
+	Q[0] =  Qin.w;
+	Q[1] = -Qin.x;
+	Q[2] = -Qin.y;
+	Q[3] = -Qin.z;
+
+
+	C11 = Q[0]*Q[0] + Q[1]*Q[1] - Q[2]*Q[2] - Q[3]*Q[3];
+	C12 = 2.0f*(Q[1]*Q[2] - Q[0]*Q[3]);
+	C13 = 2.0f*(Q[1]*Q[3] + Q[0]*Q[2]);
+
+	C21 = 2.0f*(Q[1]*Q[2] + Q[0]*Q[3]);
+	C22 = Q[0]*Q[0] - Q[1]*Q[1] + Q[2]*Q[2] - Q[3]*Q[3];
+	C23 = 2.0f*(Q[2]*Q[3] - Q[0]*Q[1]);
+
+	C31 = 2.0f*(Q[1]*Q[3] - Q[0]*Q[2]);
+	C32 = 2.0f*(Q[2]*Q[3] + Q[0]*Q[1]);
+	C33 = Q[0]*Q[0] - Q[1]*Q[1] - Q[2]*Q[2] + Q[3]*Q[3];
+
+	vector_v[0] = C11*vector_e[0] + C12*vector_e[1] + C13*vector_e[2];
+	vector_v[1] = C21*vector_e[0] + C22*vector_e[1] + C23*vector_e[2];
+	vector_v[2] = C31*vector_e[0] + C32*vector_e[1] + C33*vector_e[2];
+
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "basic_control");
+  ros::NodeHandle node_handle;
+  rc_mode = 0;
+  ctrl_mode = 2;
+  pid_init();
+  BasicControl control(&node_handle);
+  return 0;
+}
+
+BasicControl::BasicControl(ros::NodeHandle *nh){
+  pwm_cmd.rotorPWM0 = 0.1;
+  pwm_cmd.rotorPWM1 = 0.1;
+  pwm_cmd.rotorPWM2 = 0.1;
+  pwm_cmd.rotorPWM3 = 0.1;
+
+  rc_channel1_suber = nh->subscribe<std_msgs::Float32>("/custom_debug/rc1", 1, std::bind(&BasicControl::channel1_callback, this, std::placeholders::_1));
+  rc_channel2_suber = nh->subscribe<std_msgs::Float32>("/custom_debug/rc2", 1, std::bind(&BasicControl::channel2_callback, this, std::placeholders::_1));
+  rc_channel3_suber = nh->subscribe<std_msgs::Float32>("/custom_debug/rc3", 1, std::bind(&BasicControl::channel3_callback, this, std::placeholders::_1));
+  rc_channel4_suber = nh->subscribe<std_msgs::Float32>("/custom_debug/rc4", 1, std::bind(&BasicControl::channel4_callback, this, std::placeholders::_1));
+  rc_channel5_suber = nh->subscribe<std_msgs::Float32>("/custom_debug/rc5", 1, std::bind(&BasicControl::channel5_callback, this, std::placeholders::_1));
+  rc_channel6_suber = nh->subscribe<std_msgs::Float32>("/custom_debug/rc6", 1, std::bind(&BasicControl::channel6_callback, this, std::placeholders::_1));
+  nh->setParam("/custom_debug/rc_mode", 1);//TODO(change to 0 later)
+  imu_suber = nh->subscribe<sensor_msgs::Imu>("/airsim_node/drone_1/imu/imu", 1, std::bind(&BasicControl::imuCallback, this, std::placeholders::_1));
+  pwm_publisher = nh->advertise<airsim_ros::RotorPWM>("/airsim_node/drone_1/rotor_pwm_cmd", 1);
+
+  rc_mode_timer = nh->createTimer(ros::Duration(0.2), std::bind(&BasicControl::rc_mode_check_callback, this, std::placeholders::_1));
+
+  ros::spin();
+}
+
+BasicControl::~BasicControl(){}
+
+void BasicControl::imuCallback(const sensor_msgs::Imu::ConstPtr& msg)
+{
+//  ROS_INFO("Received IMU data:");
+//  ROS_INFO("Linear Acceleration: [x: %f, y: %f, z: %f]", msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z);
+//  ROS_INFO("Angular Velocity: [x: %f, y: %f, z: %f]", msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z);
+//  ROS_INFO("Orientation: [x: %f, y: %f, z: %f, w: %f]", msg->orientation.x, msg->orientation.y, msg->orientation.z, msg->orientation.w);
+	gyro_data[0] = msg->angular_velocity.x;
+    gyro_data[1] = msg->angular_velocity.y;
+    gyro_data[2] = msg->angular_velocity.z;
+    if(ctrl_mode == 0 and rc_mode == 1){
+		target_velocity_roll = rc_channel[0] * 0.005;
+		target_velocity_pitch = rc_channel[1] * 0.005;
+		target_velocity_yaw = rc_channel[3] * -0.005;
+        throttle_set = (rc_channel[2] / 2.0 + 500.0)/500.0;
+    }
+
+    imu_roll = gyro_data[0];
+	imu_pitch = -gyro_data[1];
+	imu_yaw = -gyro_data[2];
+	float roll_in = kalman_roll(imu_roll);
+	float pitch_in = kalman_pitch(imu_pitch);
+	float yaw_in = kalman_yaw(imu_yaw);
+//    std::cout << "roll_in: " << roll_in << std::endl;
+	output_roll = pid_roll(target_velocity_roll, roll_in) / 500.0;
+	output_pitch = pid_pitch(target_velocity_pitch, pitch_in) / 500.0;
+	output_yaw = pid_yaw(target_velocity_yaw, yaw_in) / 500.0;
+//    std::cout<<output_roll<<" "<<output_pitch<<" "<<output_yaw<<std::endl;
+	float front_left_speed 	= 0.0 + output_roll - output_pitch + output_yaw + throttle_set;
+    float front_right_speed = 0.0 - output_roll - output_pitch - output_yaw + throttle_set;
+    float rear_left_speed 	= 0.0 + output_roll + output_pitch - output_yaw + throttle_set;
+    float rear_right_speed 	= 0.0 - output_roll + output_pitch + output_yaw + throttle_set;
+
+    if(front_left_speed < motor_idle_speed){
+      front_left_speed = motor_idle_speed;
+    }
+    if(front_right_speed < motor_idle_speed){
+      front_right_speed = motor_idle_speed;
+    }
+    if(rear_left_speed < motor_idle_speed){
+      rear_left_speed = motor_idle_speed;
+    }
+    if(rear_right_speed < motor_idle_speed){
+      rear_right_speed = motor_idle_speed;
+    }
+    if(front_left_speed > motor_max_speed){
+      front_left_speed = motor_max_speed;
+    }
+    if(front_right_speed > motor_max_speed){
+      front_right_speed = motor_max_speed;
+    }
+    if(rear_left_speed > motor_max_speed){
+      rear_left_speed = motor_max_speed;
+    }
+    if(rear_right_speed > motor_max_speed){
+      rear_right_speed = motor_max_speed;
+    }
+
+	pwm_cmd.rotorPWM0 = front_right_speed;
+	pwm_cmd.rotorPWM1 = rear_left_speed;
+	pwm_cmd.rotorPWM2 = front_left_speed;
+	pwm_cmd.rotorPWM3 = rear_right_speed;
+
+	pwm_publisher.publish(pwm_cmd);
+
+}
+
+void BasicControl::channel1_callback(const std_msgs::Float32::ConstPtr& msg){
+  rc_channel[0] = msg->data;
+}
+void BasicControl::channel2_callback(const std_msgs::Float32::ConstPtr& msg){
+  rc_channel[1] = msg->data;
+}
+void BasicControl::channel3_callback(const std_msgs::Float32::ConstPtr& msg){
+  rc_channel[2] = msg->data;
+}
+void BasicControl::channel4_callback(const std_msgs::Float32::ConstPtr& msg){
+  rc_channel[3] = msg->data;
+}
+void BasicControl::channel5_callback(const std_msgs::Float32::ConstPtr& msg){
+  rc_channel[4] = msg->data;
+}
+void BasicControl::channel6_callback(const std_msgs::Float32::ConstPtr& msg){
+  rc_channel[5] = msg->data;
+}
+
+void BasicControl::rc_mode_check_callback(const ros::TimerEvent& event){
+  int got_mode;
+  ros::NodeHandle nh;
+  nh.getParam("/custom_debug/rc_mode", got_mode);
+  if(got_mode == 0){
+    ctrl_mode = 2;
+    rc_mode = 0;
+  }else{
+    rc_mode = 1;
+    switch(got_mode){
+      case 1:
+        ctrl_mode = 0;
+        break;
+      case 2:
+        ctrl_mode = 1;
+        break;
+      case 3:
+       	ctrl_mode = 2;
+        break;
+      default:
+        ctrl_mode = 1;
+        break;
+    }
+  }
+//  std::cout<<ctrl_mode<<" "<<rc_mode <<std::endl;
+}
